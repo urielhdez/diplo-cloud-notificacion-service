36abfb54ba2ca8fd68c49bad145a31eeca1fc8a3
Alberto Farias <cafaray@gmail.com>
2023-12-03 23:14:06 +0100

    Probing tekton triggers

--- scripts/deployment.sh
+++ scripts/deployment.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
 
-# $1 Deployment name
-# $2 Image
+# if [[ $# -ne 2 ]]; then
+#     echo 'Too many/few arguments, expecting two' >&2
+#     echo 'ex. deployment.sh my-nginx docker.io/nginx:latest' >&2
+#     exit 1
+# fi
+DEPLOYMENT_NAME="notificaiones-service"
+IMAGE_FULL_NAME="docker.io/cafaray/notificaciones:V7"
 
-DEPLOYMENT_NAME=$1
-IMAGE=$2
-
-echo "Working with values: DEPLOYMENT_NAME = $DEPLOYMENT_NAME & IMAGE = $IMAGE \n"
-echo "starting deployment \n"
+echo "Working with values: DEPLOYMENT_NAME = $DEPLOYMENT_NAME & IMAGE = $IMAGE_FULL_NAME"
+echo "starting deployment"
 kubectl delete deployment $DEPLOYMENT_NAME
-kubectl create deployment $DEPLOYMENT_NAME --replicas=2 --image=$IMAGE
-echo "verifying deployment \n"
+kubectl create deployment $DEPLOYMENT_NAME --replicas=2 --image=$IMAGE_IMAGE_FULL_NAME
+echo "verifying deployment"
 CURRENT_ALREADY_PODS=$(kubectl get deployments $DEPLOYMENT_NAME | awk 'NR==2{print $4}')
 MINIMAL_AVAILABLE_PODS=2
 COUNTER=1
 while [ $((CURRENT_ALREADY_PODS)) -lt $((MINIMAL_AVAILABLE_PODS)) ]
   do
-    echo "waiting for pods \n"
+    echo "waiting for pods"
     CURRENT_ALREADY_PODS=$(kubectl get deployments $DEPLOYMENT_NAME | awk 'NR==2{print $4}')
     COUNTER=$(( $COUNTER + 1 ))
     if [ $COUNTER -gt 20 ]
     then
-      echo "It looks something went wrong with the deployment $DEPLOYMENT_NAME. I can not wait more ... \n"
+      echo "It looks something went wrong with the deployment $DEPLOYMENT_NAME. I can not wait more ..."
       exit 1
     else
       sleep 3
     fi
 done;
-echo "deployment ready, here the status: \n"
+echo "deployment ready, here the status:"
 kubectl get deployment $DEPLOYMENT_NAME -o wide
